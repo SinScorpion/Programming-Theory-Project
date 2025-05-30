36c0aab244b36e2e05f4e02d1183a706c372e15e
SinScorpion <sinscorpion90@gmail.com>
2025-05-30 13:50:33 +0700

    new scene

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+
+    public void StartScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
